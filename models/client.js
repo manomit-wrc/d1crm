c4f032070e373f8fe4131e688c3f6a207fc676fc
Manomit Mitra <manomit@wrctechnologies.com>
2017-10-11 20:04:39 +0530

    commit

---
+++ models/client.js
@@ -0,0 +1,35 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var clientSchema = new Schema({
+	first_name: {type: String, required: 'Please enter first name'},
+	last_name: {type:String, required: 'Please enter last name'},
+	email: {type:String, required: 'Please enter email id'},
+	password: {type:String, required: 'Please enter password'},
+	mobile_no: {type:String, required: 'Please enter mobile no'},
+	address: {type:String, default: ''},
+	country_name: {type:String, default: ''},
+	state_name: {type:String, default: ''},
+	city_name: {type:String, default: ''},
+	pincode: {type:String, default: ''},
+	device_id: {type:String, required: 'Please provide device id'},
+	platform: {type:String, required: 'Please provide platform'}
+});
+
+clientSchema.path('email').validate(function(email, next){
+
+Client.findOne({email:email.toLowerCase()}, function(err, client){
+ if(err) {
+      return next(false);
+  }
+  next(!client);
+});
+
+},'Email already in use');
+
+var Client = mongoose.model('Client', clientSchema);
+
+module.exports = {
+	Client: Client
+}
